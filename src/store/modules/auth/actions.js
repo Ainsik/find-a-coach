937eeafcf6eb82b45058a39fe947ad392f755cee
Ainsik <pio.andrzejczuk@gmail.com>
2023-06-05 13:59:26 +0200

    adds singup action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,32 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    console.log('dupa');
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDGtcAkMx-vfJ2DD39B0mkXPZYLRZADkRw",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || "Failed to authenticate."
+      );
+      throw error;
+    }
+    console.log(responseData);
+
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
